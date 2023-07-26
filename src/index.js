bc62bb46c0fa9486fe27301735e3828be811600c
Amina <amina0ismail0@gmail.com>
2023-07-26 13:17:04 +0200

    Added 5 day forecast

--- src/index.js
+++ src/index.js
@@ -140,7 +140,7 @@ function displayForecast(response) {
   let forecastHTML = `<div class="row">`;
 
   forecast.forEach(function (forecastDay, index) {
-    if (index < 6) {
+    if (index > 0 && index < 6) {
       forecastHTML =
         forecastHTML +
         ` <div class="col">
